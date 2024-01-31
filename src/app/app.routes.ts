97d8bdbcddfae03d1109fedd1b475e33b5108313
lahl82 <luisadolfo.hernandezlara@gmail.com>
2024-01-31 17:54:37 -0400

    add basic routes to brose the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ServiceDetailComponent } from './components/service-detail/service-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServiceComponent },
+  { path: 'services/:serviceId', component: ServiceDetailComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
