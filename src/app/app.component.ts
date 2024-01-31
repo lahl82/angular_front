ed40b3b0781084fbc76f85b2ab3abf16b3a3afb8
lahl82 <luisadolfo.hernandezlara@gmail.com>
2024-01-31 17:57:34 -0400

    add a basic main menu with navbar from bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { ServiceComponent } from './components/service/service.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceDetailComponent } from './components/service-detail/service-detail.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    ServiceComponent,
+    ServiceDetailComponent,
+    RegisterComponent,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
